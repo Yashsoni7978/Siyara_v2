c8f806d02f3f041860cb3547470c1b4a578f6372
Developer <developer@example.com>
2026-08-30 16:31:38 +0530

    Fix GitHub Pages deployment for Next.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
   images: {
+    unoptimized: true,
     // Let's support loading images from Unsplash or other external domains if next/image is used
     remotePatterns: [
       {
